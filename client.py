4525f625308ae9ce99f9b09e274394b8224f662c
y-du <42994541+y-du@users.noreply.github.com>
2017-10-25 14:42:45 +0200

    added connector-client import path

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+import os, sys, inspect
+import_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"connector_client")))
+if import_path not in sys.path:
+    sys.path.insert(0, import_path)
+
 try:
     from modules.logger import root_logger
     from modules.http_lib import Methods as http
