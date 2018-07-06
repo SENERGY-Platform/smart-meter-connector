db16d4d123eb9dec7f4a6db0560668a224ff2a6b
y-du <42994541+y-du@users.noreply.github.com>
2018-07-06 15:54:44 +0200

    use connector-client package instead of submodule

--- client.py
+++ client.py
@@ -1,18 +1,13 @@
-import os, sys, inspect
-import_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"connector_client")))
-if import_path not in sys.path:
-    sys.path.insert(0, import_path)
-
 try:
-    from modules.http_lib import Methods as http
-    from modules.device_pool import DevicePool
-    from connector.client import Client
-    from connector.device import Device
+    from connector_client.modules.http_lib import Methods as http
+    from connector_client.modules.device_pool import DevicePool
+    from connector_client.client import Client
+    from connector_client.device import Device
     from smart_meter_serial import SmartMeterSerial
     from logger import root_logger
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
-import datetime, json, threading
+import datetime, json
 
 
 logger = root_logger.getChild(__name__)
