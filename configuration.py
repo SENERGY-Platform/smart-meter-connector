6ff2166bfefe0516e3224cab74f2db9827443168
y-du <42994541+y-du@users.noreply.github.com>
2019-07-15 15:11:52 +0200

    use simple-conf module for configuration

--- configuration.py
+++ configuration.py
@@ -17,47 +17,35 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
-import os, inspect, configparser
 
-conf_path = os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0])
-conf_file = 'sm.conf'
-
-config = configparser.ConfigParser()
+from simple_conf import configuration, section
+from os import getcwd
 
 
-if not os.path.isfile(os.path.join(conf_path, conf_file)):
-    print('No config file found')
-    config['SMART_METER'] = {
-        'id': '',
-        'name': '',
-        'manufacturer': '',
-        'type': ''
-    }
-    config['SEPL'] = {
-        'device_type': '',
-        'device_service': ''
-    }
-    with open(os.path.join(conf_path, conf_file), 'w') as cf:
-        config.write(cf)
-    exit("Created blank config file at '{}'".format(conf_path))
+@configuration
+class SMConf:
+
+    @section
+    class SmartMeter:
+        id = None
+        name = None
+        manufacturer = None
+
+    @section
+    class Senergy:
+        dt_sm = None
+        st_sm = None
+
+    @section
+    class Logger:
+        level = "info"
 
 
-try:
-    config.read(os.path.join(conf_path, conf_file))
-except Exception as ex:
-    exit(ex)
+config = SMConf('sm.conf', getcwd())
 
 
-SM_ID = config['SMART_METER']['id']
-SM_NAME = config['SMART_METER']['name']
-SM_MANUFACTURER = config['SMART_METER']['manufacturer']
-SM_TYPE = config['SMART_METER']['type']
-SEPL_DEVICE_TYPE = config['SEPL']['device_type']
-SEPL_SERVICE = config['SEPL']['device_service']
+if not all((config.SmartMeter.id, config.SmartMeter.name, config.SmartMeter.manufacturer)):
+    exit('Please provide Smart Meter information')
 
-
-if not (SM_ID and SM_NAME):
-    exit('Please provide a smart meter ID and name')
-
-if not (SEPL_DEVICE_TYPE and SEPL_SERVICE):
-    exit('Please provide a SEPL device type and service')
+if not all((config.Senergy.dt_sm, config.Senergy.st_sm)):
+    exit('Please provide a SENERGY device and service types')
