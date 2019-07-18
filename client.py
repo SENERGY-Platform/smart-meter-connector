39aa615cbd09485c1b198ff496b407463e0bbcca
y-du <42994541+y-du@users.noreply.github.com>
2019-07-18 11:25:57 +0200

    only use one timestamp

--- client.py
+++ client.py
@@ -36,18 +36,16 @@ class GetMeasurements(cc_lib.types.SensorService):
     def task(source):
         reading = source.read()
         if reading:
-            timestamp = '{}Z'.format(datetime.datetime.utcnow().isoformat())
             return {
                 "1_8_0": {
                     "value": float(reading["1.8.0"][0]),
-                    "unit": reading["1.8.0"][1],
-                    "time": timestamp
+                    "unit": reading["1.8.0"][1]
                 },
                 "16_7": {
                     "value": float(reading["16.7"][0]),
-                    "unit": reading["16.7"][1],
-                    "time": timestamp
-                }
+                    "unit": reading["16.7"][1]
+                },
+                "time": '{}Z'.format(datetime.datetime.utcnow().isoformat())
             }
 
 
